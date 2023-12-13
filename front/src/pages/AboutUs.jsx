59e04146ac1a015b3c58c4a9489b959f421df2d4
gretchenhu <huiqinhu88@gmail.com>
2023-12-12 17:01:12 -0800

    edit about us

--- front/src/pages/AboutUs.jsx
+++ front/src/pages/AboutUs.jsx
@@ -28,49 +28,8 @@ const AboutUs = () => {
             generously provided us with her Buddha Artifacts dataset🧡
           </p>
         </div>
-
-        <div className="about-section">
-          <h2> 🕉️When Om-Mani-Padme-Hum meets Dot-Com</h2>
-          <p>
-            Our mission is to offer an enlightening experience that transcends
-            the traditional museum visit. We seek to connect the past with the
-            present, offering a space where ancient wisdom and modern curiosity
-            meet. We aim to educate our audience on the profound narratives
-            woven into Buddhist art, inspire a deeper appreciation for its
-            aesthetic and philosophical depth, and provoke reflection on its
-            contemporary relevance. Our carefully selected collection invites
-            visitors to traverse the breadth of Buddhist culture—from its
-            intricate iconography to its diverse expressions across different
-            lands. These artifacts, each with a story to tell, were once removed
-            from their origins and dispersed across continents in an era marked
-            by colonial endeavors. Now, they serve not only as relics of their
-            own rich historical contexts but also as symbols of resilience and
-            cultural dialogue. Despite their journeys through times of conquest
-            and appropriation, they echo the timeless Buddhist teachings of
-            serenity and the liberating power of release.
-          </p>
-        </div>
-
-        <div className="about-section">
-          <h2> 🧘🏻🧘🏻‍♀️The Guardians of the Gallery</h2>
-          <p>
-            Our gallery is more than walls and webpages—it&apos;s a gathering of
-            souls keen on history, seekers of zen, and yes, we&apos;ve got the
-            incense enthusiast who&apos;s all about that aromatic life. Our
-            founders, who once playfully confused an &quot;aha&quot; moment with
-            a well-placed floor lamp, have brought together a crew that&apos;s
-            as much at home in a dusty archive as in a lotus position.
-            We&apos;re your modern-day monastery scribes and scripture-keepers,
-            minus the robes. In our ranks, you&qpos;ll find a meditation
-            virtuoso whose &quot;oms&quot; could drown out a rock concert, and a
-            coding guru who&apos;s pretty sure a well-written algorithm is the
-            universe talking back. Together, we&apos;re on a quest to serve up
-            enlightenment with a digital twist, sharing the profound chill of
-            the Buddha beat by beat—commercial-free, as the ancients intended.
-          </p>
-        </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
